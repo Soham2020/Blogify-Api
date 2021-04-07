0f10c265c2dfefc1b86d66e2b638020d18dae132
Soham Das <majumder70.ranu@gmail.com>
2021-04-07 08:05:04 +0530

    Added blog model

---
+++ models/blogModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Blogs', blogSchema);
