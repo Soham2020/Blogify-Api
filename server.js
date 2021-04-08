2b21e85ae075fcfa4dd258b24b60585fe0839cad
Soham Das <majumder70.ranu@gmail.com>
2021-04-08 08:21:05 +0530

    minor fixing

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.use('/users', userRoute)
 server.use('/blogs', blogRoute)
 
 // PORT Connected
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server runnning at port ${PORT}`);
 });
